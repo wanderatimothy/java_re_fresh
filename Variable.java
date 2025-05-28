c0049631dda5584e16de68dd898730db7482ab6f
wandera timothy <wanderatimothy2@gmail.com>
2025-05-28 19:47:47 +0300

    update - varriables in java

---
+++ Variable.java
@@ -0,0 +1,52 @@
+class Variable {
+
+    public static void main (String[] args) {
+        //Single line comment 
+        /*
+        Multi line comment
+        */
+        int a = 10;
+        // System.out.println("");  - prints on a new line everytime
+        // System.out.print("");  - prints on the same line everytime
+
+        System.out.println(a);
+        System.out.print(a);
+
+
+        int x = 30;
+        int y = 20;
+        
+        //mutliplication 
+        System.out.println(x*y);
+        //addition
+        System.out.println(x+y);
+        //subtraction
+        System.out.println(x-y);
+        //division
+        System.out.println(x/y);
+        //modulus -  remainder of a value
+        System.out.println(x%y);
+
+        //Primitive data types
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+
+        int num = 10;
+        long num2 = 8392l;
+        short num3 =223;
+        byte by = 126;
+
+        float mum5 = 3.14f; // loose with precission
+        double num6 = 3.14d; // better with precission
+        
+        boolean old = true;
+        char c = 'z';  //No double quotes , only single quotes , only one character is allowed
+
+
+
+    }
+}
